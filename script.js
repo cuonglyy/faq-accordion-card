b1d0259b97895d07d80e835a517f0438ce18fe0d
Cuong Ly <cuonglyy@gmail.com>
2023-02-16 21:28:09 -0800

    button toggles

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const questionBtn = document.querySelectorAll('.faq-question__btn');
 const answer = document.querySelectorAll('.faq-answer__text');
 const arrowIcon = document.querySelectorAll('.arrow-icon');
+const questionContainer = document.querySelectorAll('.faq__parent');
 
+const questionBtnArr = Array.from(questionBtn);
 const answerArr = Array.from(answer);
 const arrowIconArr = Array.from(arrowIcon);
+const questionContainerArr = Array.from(questionContainer);
 
 
-const displayAnswer = () => {
-
+const displayAnswer = (e) => {
+    /*
+    questionBtn.forEach(btn => {
+        if(btn.classList.contains('active')) {
+            
+            btn.classList.remove('active');
+            btn.firstElementChild.classList.remove('active');
+            
+        }
+    })
+    */
+    e.target.classList.toggle('active');
+    e.target.firstElementChild.classList.toggle('active');
 }
 
 
