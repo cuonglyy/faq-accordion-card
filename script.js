ac0f8632e935b0ae4287224fae63453d27b6d6be
Cuong Ly <cuonglyy@gmail.com>
2023-02-20 07:59:09 -0800

    Finished mobile design

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 const questionBtn = document.querySelectorAll('.faq-question__btn');
-const answer = document.querySelectorAll('.faq-answer__text');
-const arrowIcon = document.querySelectorAll('.arrow-icon');
-const questionContainer = document.querySelectorAll('.faq__parent');
-
-const questionBtnArr = Array.from(questionBtn);
-const answerArr = Array.from(answer);
-const arrowIconArr = Array.from(arrowIcon);
-const questionContainerArr = Array.from(questionContainer);
-
 
 const displayAnswer = (e) => {
-    /*
-    questionBtn.forEach(btn => {
-        if(btn.classList.contains('active')) {
-            
-            btn.classList.remove('active');
-            btn.firstElementChild.classList.remove('active');
-            
-        }
-    })
-    */
-    e.target.classList.toggle('active');
-    e.target.firstElementChild.classList.toggle('active');
+    const btn = e.target;
+    btn.classList.toggle('active');
+    btn.firstElementChild.classList.toggle('active');
+    btn.nextElementSibling.style.display = 
+                btn.nextElementSibling.style.display === 'block' ? 'none' : 'block';
 }
-
-
-
 
 questionBtn.forEach(btn => {
     btn.addEventListener('click', displayAnswer);
